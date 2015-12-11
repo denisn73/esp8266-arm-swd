595ab12260c59f0876d10b7a6c645d12d6936cee
Micah Elizabeth Scott <micah@scanlime.org>
2015-12-10 19:15:33 -0800

    Webdev proxy is now usable

--- webdev-proxy.js
+++ webdev-proxy.js
@@ -9,11 +9,21 @@
 //
 
 var TARGET_HOST = 'esp8266-swd.local',
+    HTTP_PORT = 8266,
     SKETCH_PATH = '.',
-    HTTP_PORT = 8266;
+    TYPES = { js: 'text/javascript', html: 'text/html', css: 'text/css' };
 
 var http = require('http'),
+    path = require('path'),
+    dns = require('dns'),
     fs = require('fs');
+
+console.log('Resolving target host, ' + TARGET_HOST);
+dns.lookup(TARGET_HOST, function (err, address, family) {
+    TARGET_HOST = address; // Cache the DNS lookup
+    http.createServer(handleRequest).listen(HTTP_PORT);
+    console.log('http://localhost:' + HTTP_PORT);
+});
 
 function localFilePath (url) {
     var localPath = SKETCH_PATH + url + '.h';
@@ -24,44 +34,31 @@ function unwrapHeader(contents) {
     return String(contents).trim().split('\n').slice(1, -1).join('\n');
 }
 
-var server = http.createServer(function(req, res) {
+function handleRequest(req, res) {
     var logInfo = req.method + ' ' + req.url;
     var localPath = localFilePath(req.url);
-
     if (localPath) {
         fs.readFile(localPath, function (err, data) {
             if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            var type = TYPES[path.extname(req.url).substr(1)] || 'text/plain';
+            res.writeHead(200, {'Content-Type': type});
             res.end(unwrapHeader(data));
-            console.log(logInfo + ' <--file-- ' + localPath);
+            console.log(logInfo + ' <--file-- ' + localPath + ' (' + type + ')');
         });
-
     } else {
-        console.log(logInfo + ' proxy...');
-        http.request({
+        http.get({
             hostname: TARGET_HOST,
-            port: 80,
             path: req.url,
-            method: req.method,
             agent: false
-        }, function (proxy, err) {
-            console.log(logInfo + ' = ' + proxy.statusCode);
-
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('Proxy error');
-            } else {
-                res.writeHead(proxy.statusCode, proxy.headers);
-                proxy.on('data', function(chunk) {
-                    res.write(chunk);
-                }));
-                proxy.on('end', function() {
-                    res.end();
-                }));
-            }
-        }).end();
+        },
+        function (proxy) {
+            console.log(logInfo + ' proxy ' + proxy.statusCode);
+            res.writeHead(proxy.statusCode, proxy.headers);
+            proxy.on('data', function(chunk) { res.write(chunk); });
+            proxy.on('end', function() { res.end(); });
+        }).on('error', function (e) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Proxy error: ' + e.message);
+        });
     }
-});
- 
-server.listen(HTTP_PORT);
-console.log('http://localhost:' + HTTP_PORT);
+}
