5f4ddce7987680e058388d4661523262a76c37b8
Micah Elizabeth Scott <micah@misc.name>
2015-12-13 14:08:30 -0800

    Program example is working

--- patch/patch.cpp
+++ patch/patch.cpp
@@ -3,17 +3,20 @@
 extern "C" void _start()
 {
 	// Set up GPIOs
-	*(uint32_t*)0x40048008=0;
-	*(uint32_t*)0x400ff014=0x00300800;
+	*(volatile uint32_t*)0x40048008=0;
+	*(volatile uint32_t*)0x400ff014=0x00300800;
 
 	// Blink LEDs
 	while (1) {
 
-		*(uint32_t*)0x400ff000=0x00100800;
-		for (unsigned i = 2048; i--;) asm __volatile__ ("");
+		// Green
+		*(volatile uint32_t*)0x400ff000=0x00200800;
 
-		*(uint32_t*)0x400ff000=0x00200000;
-		for (unsigned i = 4096; i--;) asm __volatile__ ("");
+		for (unsigned i = 0x123456; i; i--) asm __volatile__ ("");
 
+		// Pink
+		*(volatile uint32_t*)0x400ff000=0x00100000;
+
+		for (unsigned i = 0x123456; i; i--) asm __volatile__ ("");
 	}
 }
